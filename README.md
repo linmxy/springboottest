f5a8d532969dc31d1afe6bcbcdbe6f71c31cd066
Mike Lin <linmxy@gmail.com>
2017-08-10 15:58:19 +0800

    add POST example

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #Spring + maven boot hellworld
+
+#test:
+    curl http://localhost:51515
+    curl -X POST "http://localhost:51515/greeting" -d '{"content": "test"}' -H "Content-Type: application/json"
--- src/main/java/com/mike/test/Controller.java
+++ src/main/java/com/mike/test/Controller.java
@@ -1,19 +1,28 @@
 package com.mike.test;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.web.bind.annotation.*;
+
+class Greeting {
+  @JsonProperty String content;
+}
 
 @RestController
 public class Controller {
 
-    @RequestMapping(value = "/information", method = RequestMethod.GET)
-    public String getInformation() {
-        return "The Title.";
-    }
+  @RequestMapping(value = "/information", method = RequestMethod.GET)
+  public String getInformation() {
+    return "The Title.";
+  }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String getIndex() {
-        return "hello index";
-    }
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String getIndex() {
+    return "greeting";
+  }
+
+  @RequestMapping(value = "/greeting", method = RequestMethod.POST)
+  public @ResponseBody
+  Greeting greeting(@RequestBody Greeting greeting) {
+    return greeting;
+  }
 }
